61886d159dcf2cfc0b9a18aaf19f620d86bf4ee2
silentesc <dominic.woehrle@gmail.com>
2024-10-27 00:56:18 +0200

    Basic example impl of login and register

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,20 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 
+const username = ref(null);
+const password = ref(null);
+
+const login = () => {
+    console.log("login with ", username.value, password.value);
+};
 </script>
 
 <template>
-    <h1>LoginView</h1>
+    <h1>RegisterView</h1>
+
+    <form @submit.prevent="login">
+        <p>Username: <input type="text" required v-model="username"></p>
+        <p>Password: <input type="password" v-model="password"></p>
+        <button type="submit">Login</button>
+    </form>
 </template>
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,7 +1,26 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 
+const username = ref(null);
+const password = ref(null);
+const repeatPassword = ref(null);
+
+const register = () => {
+    if (password.value !== repeatPassword.value) {
+        console.log("Passwords do not match.")
+        return;
+    }
+    console.log("register with ", username.value, password.value);
+};
 </script>
 
 <template>
     <h1>RegisterView</h1>
+
+    <form @submit.prevent="register">
+        <p>Username: <input type="text" required v-model="username"></p>
+        <p>Password: <input type="password" v-model="password"></p>
+        <p>Repeat Password: <input type="password" v-model="repeatPassword"></p>
+        <button type="submit">Register</button>
+    </form>
 </template>
